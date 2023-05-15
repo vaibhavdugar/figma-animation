d0bc43a51a56fc497c9304944d19e65c1f302203
Vaibhav <vrdugar02@gmail.com>
2023-05-15 18:09:18 +0530

    Initial Commit

--- src/main/main.js
+++ src/main/main.js
@@ -1,25 +1,38 @@
 "use strict";
 // Entry point for the Figma plugin
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+var exports = {};
 Object.defineProperty(exports, "__esModule", { value: true });
 const figma_utils_1 = require("../utils/figma-utils");
 // Function to run the plugin
 function runPlugin() {
-    // Get the selected Figma nodes or components
-    const selection = figma.currentPage.selection;
-    // Check if there are exactly 2 components selected
-    if (selection.length !== 2 || !selection.every(node => node.type === 'COMPONENT')) {
-        figma.notify('Please select exactly 2 components.');
-        return;
-    }
-    const mainComponent = selection[0];
-    const variantComponent = selection[1];
-    // Get the translation between the main and variant components
-    const translation = (0, figma_utils_1.calculateTranslation)(mainComponent, variantComponent);
-    // Generate the Smart Animation code
-    const code = (0, figma_utils_1.generateCode)(mainComponent, variantComponent, translation);
-    // Show the generated code in a modal dialog
-    figma.showUI(__html__, { width: 400, height: 200 });
-    figma.ui.postMessage({ type: 'code', code });
+  // Get the selected Figma nodes or components
+  const selection = figma.currentPage.selection;
+  // Check if there are exactly 2 components selected
+  if (
+    selection.length !== 2 ||
+    !selection.every((node) => node.type === "COMPONENT")
+  ) {
+    figma.notify("Please select exactly 2 components.");
+    return;
+  }
+  const mainComponent = selection[0];
+  const variantComponent = selection[1];
+  // Get the translation between the main and variant components
+  const translation = (0, figma_utils_1.calculateTranslation)(
+    mainComponent,
+    variantComponent
+  );
+  // Generate the Smart Animation code
+  const code = (0, figma_utils_1.generateCode)(
+    mainComponent,
+    variantComponent,
+    translation
+  );
+  // Show the generated code in a modal dialog
+  figma.showUI(__html__, { width: 400, height: 200 });
+  figma.ui.postMessage({ type: "code", code });
 }
 // Run the plugin
 runPlugin();
